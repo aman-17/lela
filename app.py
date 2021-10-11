c3c0f782bfda2268ca7a16af03fd8aedc6c542b6
aman-17 <aman.18bcn7075@vitap.ac.in>
2021-10-11 17:43:17 +0530

    v3

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from website import create_app
+from website.views import views
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'aman17'
+    return app
 
 app = create_app()
+
+app.register_blueprint(views)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,6 @@
-from flask import Flask
-from os import path
-from website import views
+# from flask import Flask
+# from os import path
+# from website import views
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'aman17'
-    
-    app.register_blueprint(views, url_prefix='/')
-    return app
+
 
