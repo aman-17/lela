90773994d36cc2600026f1b6e658160139ca6e4b
aman-17 <aman.18bcn7075@vitap.ac.in>
2022-01-30 20:41:18 +0530

    deployment issue fixed

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = create_app()
 app.register_blueprint(views)
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0",port=os.environ.get("PORT",5000))
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=os.environ.get("PORT",5000))
+    # app.run(debug=True)
 
 
